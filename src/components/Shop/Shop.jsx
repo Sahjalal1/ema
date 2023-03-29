4e026b6699557db7af25669c57acc309a5d6bf2a
rahim <mdsahjalalrahim@gmail.com>
2023-03-30 02:26:06 +0600

    Header add acb

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 
@@ -13,9 +14,30 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, [])
 
+    useEffect(()=>{
+        const stordCart = getShoppingCart();
+        const saveCart = [];
+        // step 1: get id of the addedProduct
+       for(const id in stordCart){
+        // step 2 get product from products state by using id
+        const addedProduct = products.find(product => product.id === id);
+        if(addedProduct){
+            // step 3: add quantity
+           const quantity = stordCart[id];
+            addedProduct.quantity = quantity; 
+            // step 4: add the added product to the saved cart 
+            saveCart.push(addedProduct);
+        }
+        // step 5: set the cart
+        setCart(saveCart)
+       
+       } 
+    } , [products])
+
     const handleAddToCart = (product) =>{
         const newcart = [...cart, product];
         setCart(newcart);
+        addToDb(product.id)
      }
 
     return (
