ef283458b4c44c356d58354a732f338fdb834e92
rahim <mdsahjalalrahim@gmail.com>
2023-04-29 19:26:26 +0600

    ab

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -4,6 +4,8 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../components/Provider/AuthProvider';
 
 const Login = () => {
+    const [show, setShow] = useState(false)
+
     const { signIn } = useContext(AuthContext)
     const navigate = useNavigate();
     const location = useLocation()
@@ -42,7 +44,12 @@ const Login = () => {
                 </div>
                 <div className='form-control'>
                     <label htmlFor="password">Password</label>
-                    <input type="password" name='password' required />
+                    <input type={show ? "text" : "password"} name='password' required />
+                    <p onClick={() => setShow(!show)}><small>
+                        {
+                            show ? <span>Hide Password</span> : <span>Show Password</span>
+                        }
+                    </small></p>
                 </div>
                 <input className='btn-submit' type="submit" value="Login" />
             </form>
